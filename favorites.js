9910dd10b44fdfe7e5b49154a44f75fee842bc8b
KevinikaLegier <k.legier2012@gmail.com>
2025-03-16 19:18:30 -0500

    bootstrap_lab1

---
+++ favorites.js
@@ -0,0 +1,6 @@
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+import 'bootstrap/dist/css/bootstrap.min.css';
+    import 'bootstrap/dist/js/bootstrap.min.js';
+
+    
